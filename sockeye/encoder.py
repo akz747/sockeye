75a771a458987fa0cac992ffeb942b36f1f4f84d
beckdaniel <daniel.e.beck@gmail.com>
2017-07-18 19:13:10 -0400

    encapsulated into a separate gcn code

--- sockeye/encoder.py
+++ sockeye/encoder.py
@@ -30,6 +30,8 @@ from . import convolution
 from . import transformer
 from . import utils
 
+import sockeye.gcn
+
 logger = logging.getLogger(__name__)
 
 
@@ -1132,7 +1134,7 @@ class GraphConvEncoder(Encoder):
                  fused: bool = False):
         self.layout = layout
         self.fused = fused
-        #self.gcn = sockeye.gcn.get_gcn(prefix)
+        self.gcn = sockeye.gcn.get_gcn(prefix)
 
     def encode(self, data: mx.sym.Symbol, 
                data_length: mx.sym.Symbol, seq_len: int, metadata=None):
@@ -1142,12 +1144,12 @@ class GraphConvEncoder(Encoder):
         adj = metadata
         with mx.AttrScope(__layout__=C.BATCH_MAJOR):
             data = mx.sym.swapaxes(data=data, dim1=0, dim2=1)
-        outputs = mx.sym.batch_dot(adj, data)
-        #outputs = self.gcn.convolve(data, adj)
+        #outputs = mx.sym.batch_dot(adj, data)
+        outputs = self.gcn.convolve(adj, data)
         #print(adj)
-        logger.info("I am here!")
-        logger.info(str(adj))
-        logger.info(str(data))
+        #logger.info("I am here!")
+        #logger.info(str(adj))
+        #logger.info(str(data))
         #outputs = data
         with mx.AttrScope(__layout__=C.TIME_MAJOR):
             outputs = mx.sym.swapaxes(data=outputs, dim1=0, dim2=1)
