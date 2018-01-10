dad811e43e03122969f1817005922965bc90986c
Daniel Beck <daniel.e.beck@gmail.com>
2018-01-10 14:20:04 +1100

    blah

--- data/toy2/decode.sh
+++ data/toy2/decode.sh
@@ -1,1 +1,1 @@
-python -m sockeye.translate -m toy_model --edge-vocab data/toy2/edge_vocab.json --use-cpu --max-input-len 40 < data/toy2/val.en.tokdeps
+python -m sockeye.translate -m toy_model --edge-vocab data/toy2/edge_vocab.json --use-cpu --max-input-len 20 < data/toy2/val.en.tokdeps
--- sockeye/inference.py
+++ sockeye/inference.py
@@ -91,7 +91,7 @@ class InferenceModel(model.SockeyeModel):
         :param max_input_length: Maximum input length.
         :param get_max_output_length_function: Callable to compute maximum output length.
         """
-        self.max_input_length = int(max_input_length / 2)
+        self.max_input_length = max_input_length
         logger.info(self.max_input_length)
         if self.max_input_length > self.training_max_seq_len_source:
             logger.warning("Model was only trained with sentences up to a length of %d, "
@@ -101,7 +101,7 @@ class InferenceModel(model.SockeyeModel):
 
         # check the maximum supported length of the encoder & decoder:
         if self.max_supported_seq_len_source is not None:
-            utils.check_condition(self.max_input_length <= self.max_supported_seq_len_source,
+            utils.check_condition(self.max_input_length / 2 <= self.max_supported_seq_len_source,
                                   "Encoder only supports a maximum length of %d" % self.max_supported_seq_len_source)
         if self.max_supported_seq_len_target is not None:
             decoder_max_len = self.get_max_output_length(max_input_length)
